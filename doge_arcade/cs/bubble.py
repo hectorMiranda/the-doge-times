ff1e3a364de52011d256de97afea7cfb37640cff
Hector Miranda <marcetux@gmail.com>
2023-12-11 16:55:31 -0800

    adding visual elements

--- doge_arcade/cs/bubble.py
+++ doge_arcade/cs/bubble.py
@@ -17,6 +17,10 @@ class Book(arcade.Sprite):
         self.center_y = self.height / 2 + 100  # Offset from bottom of the screen
         self.color = arcade.color.BLUE
 
+        # Target positions for animation
+        self.target_x = position_x
+        self.target_y = self.center_y
+
     def draw_book(self):
         # Draw the book as a rectangle
         arcade.draw_rectangle_filled(self.center_x, 
@@ -24,6 +28,19 @@ class Book(arcade.Sprite):
                                      self.width, 
                                      self.height, 
                                      self.color)
+        
+         # Draw the value on the book
+        text_x = self.center_x
+        text_y = self.center_y - 7  # Adjust the Y position to center the text
+        arcade.draw_text(str(self.value), text_x, text_y, arcade.color.WHITE, 12, 
+                         width=self.width, align="center", anchor_x="center", anchor_y="center")
+
+    def update_animation(self, delta_time: float = 1/60):
+        if self.center_x != self.target_x:
+            self.center_x = arcade.lerp(self.center_x, self.target_x, delta_time * 5)  # Adjust 5 for speed
+
+        if self.center_y != self.target_y:
+            self.center_y = arcade.lerp(self.center_y, self.target_y, delta_time * 5)  # Adjust 5 for speed
 
 class SortQuestGame(arcade.Window):
     """
@@ -36,6 +53,7 @@ class SortQuestGame(arcade.Window):
         self.move_count = 0
         self.is_sorted = False
         self.current_level = 1
+        self.dragged_book_shadow = None  # Shadow of the book being dragged
         self.setup()
 
     def setup(self):
@@ -62,6 +80,8 @@ class SortQuestGame(arcade.Window):
             arcade.draw_text(f"Level {self.current_level}: Sort the books!", 10, SCREEN_HEIGHT - 40, arcade.color.WHITE, 14)
         else:
             arcade.draw_text("Sorted! Click to proceed to the next level!", 10, SCREEN_HEIGHT - 40, arcade.color.GREEN, 14)
+        if self.dragged_book_shadow:
+            self.dragged_book_shadow.draw()
 
     def on_update(self, delta_time):
         """
@@ -70,7 +90,11 @@ class SortQuestGame(arcade.Window):
         if not self.is_sorted:
             self.check_sort()
         else:
+            print("Books are sorted!")  # Placeholder action
             self.setup_next_level()
+
+        for book in self.book_list:
+            book.update_animation(delta_time)
 
     def check_sort(self):
         """
@@ -94,22 +118,27 @@ class SortQuestGame(arcade.Window):
         if books:
             # Select the topmost book
             self.selected_book = books[0]
+             # Create a shadow effect for the book being dragged
+            self.dragged_book_shadow = arcade.SpriteSolidColor(self.selected_book.width, self.selected_book.height, arcade.color.GRAY)
+    
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        if self.selected_book:
+            # Update the position of the shadow effect
+            self.dragged_book_shadow.center_x = x
+            self.dragged_book_shadow.center_y = y
 
     def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when the user releases a mouse button.
-        """
         if self.selected_book:
-            # Find the closest book to swap positions with
             closest_book = min((book for book in self.book_list if book != self.selected_book),
-                               key=lambda book: abs(book.center_x - x))
+                            key=lambda book: abs(book.center_x - x))
 
-            # Swap positions of the books
-            self.selected_book.center_x, closest_book.center_x = closest_book.center_x, self.selected_book.center_x
+            # Set target positions for animation
+            self.selected_book.target_x, closest_book.target_x = closest_book.center_x, self.selected_book.center_x
             
             self.move_count += 1
-
             self.selected_book = None
+            self.dragged_book_shadow = None
+
 
 
 def main():
