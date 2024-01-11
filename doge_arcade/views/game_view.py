5037490a9cc99ea779b5086ed391b29f094c6d45
Hector Miranda <marcetux@hotmail.com>
2024-01-11 14:27:53 -0600

    Refactor variable names for clarity and consistency
    This commit refactors variable names in the codebase to improve clarity and maintain consistency.

--- doge_arcade/views/game_view.py
+++ doge_arcade/views/game_view.py
@@ -1,7 +1,7 @@
 import arcade
 from arcade import View
 import random
-from settings.config import ASSETS_PATH, PLAYER_JUMP_SPEED, TILE_SCALING, GRAVITY, PLAYER_MOVEMENT_SPEED, PLAY_MUSIC_ON_START, INITIAL_MUSIC_VOLUME, SOUND_ON, SOUND_ON_VOLUME, LAYER_NAME_PLATFORMS, LAYER_NAME_COINS, LAYER_NAME_DONT_TOUCH, GRID_PIXEL_SIZE, LAYER_NAME_BACKGROUND, LAYER_NAME_LADDERS, PLAYER_START_X, PLAYER_START_Y, LAYER_NAME_MOVING_PLATFORMS
+import settings.config as cfg
 from utilities.doge_data_hub_client import DogeDataHub  
 from UI.status_bar import StatusBar  
 from entities.player_character import PlayerCharacter
@@ -30,22 +30,22 @@ class GameView(View):
         self.clouds = self.create_clouds(10)        
         self.doge_price = "Loading..."
         self.status_bar = StatusBar(screen_width=self.display_width, bar_height=50)
-        self.status_bar.add_stat_box("Doge stats", str(ASSETS_PATH / "UI" / "price.png"))
-        self.status_bar.add_stat_box("Settings", str(ASSETS_PATH / "UI" / "settings.png"))
-        self.status_bar.add_stat_box(f"Time: {self.time_display}", str(ASSETS_PATH / "UI" / "start.png"))
-        self.status_bar.add_stat_box("Coins: NA", str(ASSETS_PATH / "UI" / "coin.png"))
-        self.status_bar.add_menu_option(0, f"Price: {DogeDataHub.doge_price}", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "start.png"))
-        self.status_bar.add_menu_option(0, "Heal", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "start.png"))
-        #self.status_bar.add_menu_option(0, "Setup wallet", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "wallet.png")) #TODO: fix 3rd item position bug
-        self.background_music = arcade.load_sound(str(ASSETS_PATH / "sounds" / "main_theme.wav"))
+        self.status_bar.add_stat_box("Doge stats", str(cfg.ASSETS_PATH / "UI" / "price.png"))
+        self.status_bar.add_stat_box("Settings", str(cfg.ASSETS_PATH / "UI" / "settings.png"))
+        self.status_bar.add_stat_box(f"Time: {self.time_display}", str(cfg.ASSETS_PATH / "UI" / "start.png"))
+        self.status_bar.add_stat_box("Coins: NA", str(cfg.ASSETS_PATH / "UI" / "coin.png"))
+        self.status_bar.add_menu_option(0, f"Price: {DogeDataHub.doge_price}", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "start.png"))
+        self.status_bar.add_menu_option(0, "Heal", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "start.png"))
+        #self.status_bar.add_menu_option(0, "Setup wallet", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "wallet.png")) #TODO: fix 3rd item position bug
+        self.background_music = arcade.load_sound(str(cfg.ASSETS_PATH / "sounds" / "main_theme.wav"))
         self.background_music_player = None
-        self.status_bar.add_menu_option(1, "sound on", self.toggle_music(), str(ASSETS_PATH / "UI" / "start.png"))
-        self.status_bar.add_menu_option(1, "Recharge", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "start.png"))        
-        self.status_bar.add_menu_option(3, "Recharge", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "start.png"))
-        self.jump_sound = arcade.load_sound(str(ASSETS_PATH / "sounds" / "bark.wav"))        
-        self.background = arcade.load_texture(str(ASSETS_PATH / "backgrounds" / "hills.png"))
-        self.game_over = arcade.load_sound(str(ASSETS_PATH / "sounds" / "hurt.wav"))
-        self.collect_coin_sound = arcade.load_sound(str(ASSETS_PATH / "sounds" / "collectable.wav"))
+        self.status_bar.add_menu_option(1, "sound on", self.toggle_music(), str(cfg.ASSETS_PATH / "UI" / "start.png"))
+        self.status_bar.add_menu_option(1, "Recharge", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "start.png"))        
+        self.status_bar.add_menu_option(3, "Recharge", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "start.png"))
+        self.jump_sound = arcade.load_sound(str(cfg.ASSETS_PATH / "sounds" / "bark.wav"))        
+        self.background = arcade.load_texture(str(cfg.ASSETS_PATH / "backgrounds" / "hills.png"))
+        self.game_over = arcade.load_sound(str(cfg.ASSETS_PATH / "sounds" / "hurt.wav"))
+        self.collect_coin_sound = arcade.load_sound(str(cfg.ASSETS_PATH / "sounds" / "collectable.wav"))
         self.player_sprite = None
         self.physics_engine = None
         self.scene = None
@@ -63,27 +63,27 @@ class GameView(View):
         self.camera = arcade.Camera(self.display_width, self.display_height)
         self.gui_camera = arcade.Camera(self.display_width, self.display_height)
           
-        map_name = f"{ASSETS_PATH}/maps/map_level_{self.level}.json"
+        map_name = f"{cfg.ASSETS_PATH}/maps/map_level_{self.level}.json"
         layer_options = {
-        LAYER_NAME_PLATFORMS: {
+        cfg.LAYER_NAME_PLATFORMS: {
             "use_spatial_hash": True,
         },
-        LAYER_NAME_MOVING_PLATFORMS: {
+        cfg.LAYER_NAME_MOVING_PLATFORMS: {
                 "use_spatial_hash": False,
         },
-        LAYER_NAME_LADDERS: {
+        cfg.LAYER_NAME_LADDERS: {
                 "use_spatial_hash": True,
         },
-        LAYER_NAME_COINS: {
+        cfg.LAYER_NAME_COINS: {
             "use_spatial_hash": True,
         },
-        LAYER_NAME_DONT_TOUCH: {
+        cfg.LAYER_NAME_DONT_TOUCH: {
             "use_spatial_hash": True,
         }
         }
        
         try:
-            self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+            self.tile_map = arcade.load_tilemap(map_name, cfg.TILE_SCALING, layer_options)
         except Exception as e:
             print(f"Error loading tile map: {e}")
     
@@ -93,10 +93,10 @@ class GameView(View):
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
         
         self.player_sprite = PlayerCharacter()
-        self.player_sprite.center_x = PLAYER_START_X
-        self.player_sprite.center_y = PLAYER_START_Y
+        self.player_sprite.center_x = cfg.PLAYER_START_X
+        self.player_sprite.center_y = cfg.PLAYER_START_Y
         
-        self.scene.add_sprite_list_after("Player", LAYER_NAME_BACKGROUND)
+        self.scene.add_sprite_list_after("Player", cfg.LAYER_NAME_BACKGROUND)
 
 
         self.scene.add_sprite("Player", self.player_sprite)
@@ -107,24 +107,24 @@ class GameView(View):
         
         self.physics_engine = arcade.PhysicsEnginePlatformer(
         self.player_sprite,
-        platforms=self.scene[LAYER_NAME_MOVING_PLATFORMS],
-        gravity_constant=GRAVITY,
-        ladders=self.scene[LAYER_NAME_LADDERS],
-        walls=self.scene[LAYER_NAME_PLATFORMS]
+        platforms=self.scene[cfg.LAYER_NAME_MOVING_PLATFORMS],
+        gravity_constant=cfg.GRAVITY,
+        ladders=self.scene[cfg.LAYER_NAME_LADDERS],
+        walls=self.scene[cfg.LAYER_NAME_PLATFORMS]
         )
                     
         if self.reset_score:
             self.score = 0
         self.reset_score = True
         
-        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
+        self.end_of_map = self.tile_map.width * cfg.GRID_PIXEL_SIZE
         self.player_sprite.isAlive=True
 
         
     def toggle_music(self):
         if self.background_music_player is None:
-            self.background_music_player = self.background_music.play(volume=INITIAL_MUSIC_VOLUME, loop=True)
-            if PLAY_MUSIC_ON_START == False:
+            self.background_music_player = self.background_music.play(volume=cfg.INITIAL_MUSIC_VOLUME, loop=True)
+            if cfg.PLAY_MUSIC_ON_START == False:
                 self.background_music_player.pause()
         else:
             if self.background_music_player.playing:
@@ -150,14 +150,14 @@ class GameView(View):
     def draw_trees(self):
         hill_bottom = 0
         hill_height = self.display_height /1.5
-        trees_texture = arcade.load_texture(str(ASSETS_PATH / "environment" / "group_of_trees.png"))
+        trees_texture = arcade.load_texture(str(cfg.ASSETS_PATH / "environment" / "group_of_trees.png"))
         center_y = hill_bottom + hill_height / 2
         arcade.draw_texture_rectangle(center_x=self.display_width / 2, center_y=center_y, width=self.display_width, height=hill_height, texture=trees_texture)
         
     def draw_house(self):
         house_bottom = 0
         house_height = self.display_height /1
-        house_texture = arcade.load_texture(str(ASSETS_PATH / "environment" / "doge_house.png"))
+        house_texture = arcade.load_texture(str(cfg.ASSETS_PATH / "environment" / "doge_house.png"))
         center_y = house_bottom + house_height / 2
         arcade.draw_texture_rectangle(center_x=self.display_width / 2, 
                                     center_y=center_y, 
@@ -168,7 +168,7 @@ class GameView(View):
     def draw_tree(self):
         tree_bottom = 200
         tree_height = 600
-        tree_texture = arcade.load_texture(str(ASSETS_PATH / "environment" / "tree_0.png"))
+        tree_texture = arcade.load_texture(str(cfg.ASSETS_PATH / "environment" / "tree_0.png"))
         center_y = tree_bottom + tree_height / 2
         arcade.draw_texture_rectangle(center_x=350, center_y=500, width=600, height=tree_height, texture=tree_texture)
 
@@ -192,14 +192,14 @@ class GameView(View):
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                self.player_sprite.change_y = cfg.PLAYER_JUMP_SPEED
                 self.jump_sound.play(volume=0.2)
         elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+            self.player_sprite.change_y = -cfg.PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.player_sprite.change_x = -cfg.PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            self.player_sprite.change_x = cfg.PLAYER_MOVEMENT_SPEED
             self.player_sprite.isAlive = True
             self.start_timer = True
         elif key == arcade.key.Z:
@@ -242,7 +242,7 @@ class GameView(View):
         self.status_bar.update_stat_box(1,f"Level {self.level}")
         self.status_bar.update_stat_box(2,f"Time: {self.time_display}")
         self.status_bar.update_stat_box(3,f"{self.score}")
-        self.status_bar.update_menu_item(0,0,f"Doge Price: {DogeDataHub.doge_price}", self.status_bar.dummy_action, str(ASSETS_PATH / "UI" / "wallet.png"))
+        self.status_bar.update_menu_item(0,0,f"Doge Price: {DogeDataHub.doge_price}", self.status_bar.dummy_action, str(cfg.ASSETS_PATH / "UI" / "wallet.png"))
         self.scene.draw()
         self.gui_camera.use()
         self.status_bar.draw()
@@ -263,7 +263,7 @@ class GameView(View):
              self.player_sprite.update_animation(delta_time)
              
         coin_hit_list = arcade.check_for_collision_with_list(
-            self.player_sprite, self.scene[LAYER_NAME_COINS]
+            self.player_sprite, self.scene[cfg.LAYER_NAME_COINS]
         )
 
         # Loop through each coin we hit (if any) and remove it
@@ -279,19 +279,19 @@ class GameView(View):
         
                # Did the player fall off the map?
         if self.player_sprite.center_y < -100:
-            self.player_sprite.center_x = PLAYER_START_X
-            self.player_sprite.center_y = PLAYER_START_Y
+            self.player_sprite.center_x = cfg.PLAYER_START_X
+            self.player_sprite.center_y = cfg.PLAYER_START_Y
 
             arcade.play_sound(self.game_over)
             self.player_sprite.isAlive = False
 
         # Did the player touch something they should not?
         if self.level <=2:
-            if arcade.check_for_collision_with_list(self.player_sprite, self.scene[LAYER_NAME_DONT_TOUCH]):
+            if arcade.check_for_collision_with_list(self.player_sprite, self.scene[cfg.LAYER_NAME_DONT_TOUCH]):
                 self.player_sprite.change_x = 0
                 self.player_sprite.change_y = 0
-                self.player_sprite.center_x = PLAYER_START_X
-                self.player_sprite.center_y = PLAYER_START_Y
+                self.player_sprite.center_x = cfg.PLAYER_START_X
+                self.player_sprite.center_y = cfg.PLAYER_START_Y
 
                 arcade.play_sound(self.game_over)
                 self.player_sprite.isAlive = False
