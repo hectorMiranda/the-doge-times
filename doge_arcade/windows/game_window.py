5ca434d300b80096c68e0e27d3f20a52e24945be
Hector Miranda <marcetux@hotmail.com>
2024-01-15 16:01:21 -0600

    Delete del.py and update GameWindow class

--- doge_arcade/windows/game_window.py
+++ doge_arcade/windows/game_window.py
@@ -4,7 +4,6 @@ import settings.config as cfg
 from utilities.doge_logger import DogeLogger
 import time
 
-
 class GameWindow(arcade.Window):
     def __init__(self):
         display_width, display_height = arcade.get_display_size()
@@ -15,8 +14,7 @@ class GameWindow(arcade.Window):
         self.fps_count = 0
         self.memory_usage = 0
         self.start_time = time.time()
-        self.fps = 0  # Initialize fps here
-        
+        self.fps = 0 
 
     def on_update(self, delta_time):
         super().on_update(delta_time)
@@ -30,13 +28,10 @@ class GameWindow(arcade.Window):
                 self.start_time = current_time
             self.memory_usage = psutil.Process().memory_info().rss / 1024 ** 2  # Memory in MB
 
-
     def on_draw(self):
         if cfg.WINDOW_PERFORMANCE_METRICS:
-            fps_text = f"FPS: {self.fps:.2f}"
-            arcade.draw_text(fps_text, 10, self.height - 20, arcade.color.WHITE, 12)
-            memory_text = f"Memory: {self.memory_usage:.2f} MB"
-            arcade.draw_text(memory_text, 10, self.height - 40, arcade.color.WHITE, 12)
+            arcade.draw_text(f"FPS: {self.fps:.2f}", 10, self.height - 20, arcade.color.WHITE, 12)
+            arcade.draw_text(f"Memory: {self.memory_usage:.2f} MB", 10, self.height - 40, arcade.color.WHITE, 12)
         
         super().on_draw()
 
